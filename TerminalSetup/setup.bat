48a7839fff4599dd382a96596672a485eaa60fc8
Satya <venkata.satya2910@gmail.com>
2023-01-04 22:34:50 +0530

    minor changes

--- TerminalSetup/setup.bat
+++ TerminalSetup/setup.bat
@@ -1,4 +1,7 @@
 @REM copying the Files to the C Drive
+echo off
+cls
+title CopyFilesForTerminalCustomization
 copy /b/v ConfigFiles\powershellProfile.ps1 C:\Users\%UserName%\OneDrive\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 copy /b/v ConfigFiles\oh-my-posh-theme.json C:\Users\%UserName%\oh-my-posh\theme.json
 echo Copying Complete
